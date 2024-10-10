9ae7864cd3322bf234c6b8d0a0191d641b0eaa9a
lajuniorfe <33765211+lajuniorfe@users.noreply.github.com>
2024-10-09 22:29:11 -0300

    portfolio - Ajustes nos ícones e tooltip

--- src/app/pages/sobre-mim/sobre-mim.component.ts
+++ src/app/pages/sobre-mim/sobre-mim.component.ts
@@ -3,10 +3,11 @@ import { ImageModule } from 'primeng/image';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { ChipModule } from 'primeng/chip';
+import { TooltipModule } from 'primeng/tooltip';
 @Component({
   selector: 'app-sobre-mim',
   standalone: true,
-  imports: [ImageModule, CardModule, ChipModule, ButtonModule],
+  imports: [ImageModule, CardModule, ChipModule, ButtonModule, TooltipModule],
   templateUrl: './sobre-mim.component.html',
   styleUrls: [],
 })
